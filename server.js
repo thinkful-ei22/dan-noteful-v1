9abeec90706780d6bae781becc99b3665ab46855
Daniyar Salikhov <salihovdr@gmail.com>
2018-06-25 15:00:00 -0400

    Get api/notes Endpoint

--- server.js
+++ server.js
@@ -11,8 +11,17 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+// app.get('/api/notes.:id', (req, res) => {
+
+// });
+
+
 app.listen(8080, function () {
-    console.info(`Server listening on ${this.address().port}`);
+  console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
-    console.error(err);
+  console.error(err);
 });
